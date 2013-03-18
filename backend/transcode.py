0163baf68ea69504e68ece26ae7ddc96dfec5aeb
Irvan <irvan.gill@gmail.com>
2013-03-17 21:15:33 -0400

    ..

--- backend/transcode.py
+++ backend/transcode.py
@@ -1,6 +1,5 @@
 import subprocess
 
-import os, cloudfiles
 
 def transcode(uid, mp3):
 	cmd = "avconv -i transcode/img/%(user)s/%05d.jpg -c:v libx264 -i transcode/mp3/%(filename)s.mp3 %(user)s.mp4" 
@@ -9,23 +8,25 @@ def transcode(uid, mp3):
 
 
 
-# import os
-# import cloudfiles
+import os
+import cloudfiles
 
 username = 'h6sidhu'
 apikey = '59d78d873277b643e665cea3a0139230'
 
 
-# conn = cloudfiles.get_connection(username, apikey)
-# container = conn.create_container('api_speed_test3')
-# data_list = ('test_data/%s'%x for x in os.listdir('test_data') if x.endswith('.dat'))
-# for filename in data_list:
-# 	try:
-# 		obj = container.create_object(filename)
-# 		obj.load_from_filename(filename)
-# 	except cloudfiles.errors.ResponseError, err:
-# 		print err
-# print len(container.list_objects())
+conn = cloudfiles.get_connection(username, apikey)
+container = conn.create_container('videos')
+data_list = ('test_data/%s'%x for x in os.listdir('test_data') if x.endswith('.dat'))
+try:
+	obj = container.create_object(filename)
+	obj.load_from_filename(filename)
+except cloudfiles.errors.ResponseError, err:
+	print err
+print len(container.list_objects())
+
+
+
 
 
 import sys
@@ -33,9 +34,14 @@ import os
 import httplib
 import time
 
+
+# METHOD 2 (uses standard http lib instead of rackspace api)
+
 from cf_auth import username, apikey
 
-container_name = sys.argv[1]
+container_name = 'videos'
+
+filename = "%s.mp4" % uid
 
 use_service_net = os.environ.get('USECFSERVICENET', False)
 
@@ -60,16 +66,14 @@ data_list = ('test_data/%s' % x for x in os.listdir('test_data')
              if x.endswith('.dat'))
 
 
-for filename in data_list:
-    start = time.time()
-    with open(filename, 'rb') as f:
-        conn.request('PUT', container_path + '/' + filename, body=f,
-             headers=send_headers)
-    resp = conn.getresponse()
-    resp.read()
-    if resp.status >= 300:
-        print resp.status, resp.reason, container_path + '/' + filename
-    print '%s uploaded in %.4f seconds' % (filename, (time.time()-start))
+start = time.time()
+with open(filename, 'rb') as f:
+    conn.request('PUT', container_path + '/' + filename, body=f,headers=send_headers)
+resp = conn.getresponse()
+resp.read()
+if resp.status >= 300:
+print resp.status, resp.reason, container_path + '/' + filename
+print '%s uploaded in %.4f seconds' % (filename, (time.time()-start))
 conn.close()
 
 
