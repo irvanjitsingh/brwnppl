47ce9673e7de0c5ff33e16d05dbcfb085c7829f8
Harnek Sidhu <h6sidhu@live.ca>
2013-03-19 22:22:11 -0400

    Cleaned up javascript recording events and other shit.

--- static/js/video.js
+++ static/js/video.js
@@ -6,45 +6,54 @@ function dataURItoBlob(dataURI) {
     }
     return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
 }
+    function stopVideo(event){
+        oFile.abort();
+        $("btnRecord").unbind("click");
+        $("btnStop").unbind("click");
 
+        if (videoTimer){
+            clearInterval(videoTimer);                    
+        }
+        localMediaStream.stop();
+        if (socket!=null){
+            socket.close();
+        }
+        video.src="";
 
-$(document).ready(function() {
-        //Compatibility
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+        $("#btnStart").bind("click", startVideo);
+                
+    }
 
-    var canvas = document.getElementById("canvas"),
-        context = canvas.getContext("2d"),
-        video = document.getElementById("video"),
-        btnStart = document.getElementById("btnStart"),
-        btnStop = document.getElementById("btnStop"),
-        btnRecord = document.getElementById("btnRecord"),
-        videoObj = {
-            video: true,
-            audio: true
+    function recordVideo(event){
+        $("#btnRecord").unbind("click");
+        socket=new WebSocket(host);
+        socket.onopen = function () {
+            console.log("Openened connection to websocket");
+            videoTimer = setInterval(
+            function () {
+                context.drawImage(video, 0, 0, 320, 240);
+                var data = $("#canvas").get()[0].toDataURL('image/jpeg', 0.2);
+                newblob = dataURItoBlob(data);
+                oFile.readAsBinaryString(newblob);
+            }, 40);
         };
-    window.socket=null;
-    window.host="ws://184.106.162.189:9000";
-    window.oFile=new FileReader();
-    window.frameCounter=1;
-    window.videoTimer=null;
-    window.userid=$("#userid").html();
 
-    oFile.onload = function (oFREvent){
+        socket.onerror = function(error){
+          console.log('WebSocket Error' + error);
+        };
 
-        var jsonObj={};
-        jsonObj["frame"]=frameCounter;
-        jsonObj["user"]=userid;
-        jsonObj["payload"]=btoa(oFREvent.target.result);
-        tmp=JSON.stringify(jsonObj);
-        socket.send(tmp);
-        ++frameCounter;
-    };
+        socket.onmessage=function(e){
+            console.log('Server: '+e.data);
+        };  
+    }
 
-    btnStart.addEventListener("click", function() {
-        var localMediaStream;
-
+    function startVideo(event){
+        $("#btnStart").unbind("click");
         if (navigator.getUserMedia) {
-            navigator.getUserMedia(videoObj, function(stream) {              
+            navigator.getUserMedia(videoObj, function(stream) {     
+                $("#btnStart").unbind("click");   
+                $("#btnStop").bind("click", stopVideo);
+                $("#btnRecord").bind("click", recordVideo);
                 video.src = (navigator.webkitGetUserMedia) ? window.webkitURL.createObjectURL(stream) : stream;
                 localMediaStream = stream;
                 
@@ -52,38 +61,42 @@ $(document).ready(function() {
                 console.error("Video capture error: ", error.code);
             });
 
-            btnStop.addEventListener("click", function() {
-                clearInterval(videoTimer);
-                localMediaStream.stop();
-                socket.close();
-                
-            });
-
-            btnRecord.addEventListener("click", function() {
-                socket=new WebSocket(host);
-                socket.onopen = function () {
-                    console.log("Openened connection to websocket");
-                    videoTimer = setInterval(
-                    function () {
-                        context.drawImage(video, 0, 0, 320, 240);
-                        var data = $("#canvas").get()[0].toDataURL('image/jpeg', 0.5);
-                        newblob = dataURItoBlob(data);
-                        oFile.readAsBinaryString(newblob);
-                    }, 33);
-                };
-
-                socket.onerror = function(error){
-                  console.log('WebSocket Error' + error);
-                 };
-
-                socket.onmessage=function(e){
-                    console.log('Server: '+e.data);
-                };  
-
-            });
             frameCounter=1;
         }
-    });
-        
+    }
 
-        });
+     $(document).ready(function() {
+        //Compatibility
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+
+    window.socket=null;
+    window.host="ws://192.168.1.127:9000";
+    window.oFile=new FileReader();
+    window.frameCounter=1;
+    window.videoTimer=null;
+    window.maxFrames=250; 
+
+    window.localMediaStream=null;
+    window.canvas = document.getElementById("canvas");
+    window.context = canvas.getContext("2d");
+    window.video = document.getElementById("video");
+    window.videoObj = {
+            video: true,
+            audio: false
+        };
+
+    oFile.onload = function (oFREvent){
+
+        var jsonObj={};
+        jsonObj["frame"]=frameCounter;
+        jsonObj["user"]="Harnek";
+        jsonObj["payload"]=btoa(oFREvent.target.result);
+        tmp=JSON.stringify(jsonObj);
+        socket.send(tmp);
+        ++frameCounter;
+    };
+
+    $("#btnStart").bind("click", startVideo);
+
+    $("#canvas").css({'visibility':'hidden'});
+    });
