49052b9c96f34d825344c12bf32aea03d5c97028
Harnek Sidhu <h6sidhu@live.ca>
2013-03-24 14:45:00 -0400

    updated to json

--- brwnppl/urls.py
+++ brwnppl/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
 	url(r'^oauth/$', 'fbapp.auth.oauth'),
 	url(r'^home/$', 'fbapp.core.home'),
 	#api
-	url(r'^api/1/videos/add/(?P<uid>\d+)/(?P<vid>\d+)/(?P<uri>.+)/?$', 'fbapp.api.create'),
+	url(r'^api/1/videos/add/$', 'fbapp.api.create'),
 	# url(r'^api/1/videos/delete/(?P<vid>\d+)/(?P<uid>\d+)/?$', 'fbapp.api.create'),
 	# url(r'^api/1/videos/add/(?P<vid>\d+)/(?P<uid>\d+)/?$', 'fbapp.api.create'),
 )
--- fbapp/api.py
+++ fbapp/api.py
@@ -1,12 +1,21 @@
 from fbapp.models import User, Video
 from django.http import HttpResponse
+import json as simplejson
 
 
-def create(request, uid, vid, uri):
+def create(request):
+	response={}
 	if User.objects.get(uid=uid):
-		v = Video(vid=vid, uri=uri, user=User.objects.get(uid=uid))
-		v.save()
-		response = 1
+		try: 
+			data = simplejson.loads(request.raw_post_data)
+			vid=data["vid"]
+			uri=data["uri"]
+			uid=data["uid"]
+			v = Video(vid=vid, uri=uri, user=User.objects.get(uid=uid))
+			v.save()
+			response.update({'response': '1'})
+		except Exception:
+			response.update({'response': '0'})
 	else:
-		response = 0
-	return HttpResponse(response)
+		response.update({'response': '0'})
+	return HttpResponse(simplejson.dumps(response), content_type='application/json')
