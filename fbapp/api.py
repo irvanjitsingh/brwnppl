bda18faef1c9ea2b1eff63ede5528c4b14ed83cf
Irvan <irvan.gill@gmail.com>
2013-03-24 23:12:31 -0400

    test

--- fbapp/api.py
+++ fbapp/api.py
@@ -6,17 +6,17 @@ import json as simplejson
 @csrf_exempt
 def create(request):
 	response={}
-	if User.objects.get(uid=uid):
-		try: 
-			data = simplejson.loads(request.raw_post_data)
-			vid=data["vid"]
-			uri=data["uri"]
-			uid=data["uid"]
+	try: 
+		data = simplejson.loads(request.raw_post_data)
+		vid=data["vid"]
+		uri=data["uri"]
+		uid=data["uid"]
+		if User.objects.get(uid=uid):
 			v = Video(vid=vid, uri=uri, user=User.objects.get(uid=uid))
 			v.save()
 			response.update({'response': '1'})
-		except Exception:
+		else:
 			response.update({'response': '0'})
-	else:
+	except Exception:
 		response.update({'response': '0'})
 	return HttpResponse(simplejson.dumps(response), content_type='application/json')
