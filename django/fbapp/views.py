98e01e6fcb92d59dd58bae05667e50461458e3c2
Irvan <irvan.gill@gmail.com>
2013-03-26 00:21:26 -0400

    taking out redundant code

--- django/fbapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.views.decorators.csrf import csrf_exempt
-
-def view(request):
-	text = 'Hello World!'
-	c = RequestContext(request, {'content': text})
-	return render_to_response('app.html', c)
