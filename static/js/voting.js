c23dd12cc86f1d605f4e0f7a17216c13ded46d48
Irvan <irvan.gill@gmail.com>
2013-03-27 21:31:16 -0400

    bugs

--- static/js/voting.js
+++ static/js/voting.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
                 }
             });
         }
-    });
+    });}} 
 
     $('div.vote-buttons img.vote-down').click(function() {
 
@@ -57,3 +57,4 @@ $(document).ready(function() {
             });
         }
     });
+};
